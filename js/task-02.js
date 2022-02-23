8941f539beecc9d49f1b6c61f6c9521f62275210
Serhii <0677267015@ukr.net>
2022-02-24 01:44:41 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,9 +7,25 @@ const ingredients = [
   'Condiments',
 ];
 
-ingredients.forEach(element => {
-  const item = document.createElement("li");
-  item.textContent = element;
-  item.classList.add("item");
-  document.querySelector("#ingredients").prepend(item);
-});
+// ingredients.forEach(element => {
+//   const item = document.createElement("li");
+//   item.textContent = element;
+//   item.classList.add("item");
+//   document.querySelector("#ingredients").prepend(item);
+// });
+
+//---------------------------------------------------------
+
+// const item = ingredients.map(element => `<li class="item">${element}</li>`).join(" ");
+// document.querySelector("#ingredients").insertAdjacentHTML("beforeend", item);
+
+//---------------------------------------------------------
+
+
+const item = ingredients.map(element => document.createElement("li")).join(" ");
+
+document.querySelector("#ingredients").prepend(item);
+
+document.querySelector("#ingredients").insertAdjacentHTML("beforeend", item);
+
+
